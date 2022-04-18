782ea608cf6a21a21687bec3b9d02d3f842339e2
FARIDJADDA <faridjadda750@gmail.com>
2022-04-18 10:54:44 +0200

    express install

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.writeHead(200, {
+    "content-type": "text/plain",
+  });
+  res.end("Hello World !");
+});
+
 app.listen(3000);
