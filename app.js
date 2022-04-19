ebc67c8a33f36344970aa3693b939cc0fa5dcc69
FARIDJADDA <faridjadda750@gmail.com>
2022-04-19 06:05:58 +0200

    introduce template engine Pug

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
 const path = require("path");
-const fs = require("fs");
 const express = require("express");
 const app = express();
 
-app.set("views", [
-  path.join(__dirname, "views"),
-  path.join(__dirname, "views2"),
-]);
-app.set("view engine", "toto");
-app.engine("toto", (path, options, callback) => {
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      callback(err);
-    }
-    const template = data.toString().replace("%name", options.name);
-    callback(null, template);
-  });
-});
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
   res.render("index", { name: "jean" });
