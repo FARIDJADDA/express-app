4ff6995576bfa80b9ae031dfd0f5fdcd518eb41f
FARIDJADDA <faridjadda750@gmail.com>
2022-04-19 04:32:08 +0200

    method on object response

--- app.js
+++ app.js
@@ -4,14 +4,27 @@
 // );
 // console.log(test);
 
+const path = require("path");
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.writeHead(200, {
-    "content-type": "text/plain",
-  });
-  res.end("Hello !");
+  // res.send({ key1: "value1" }); // send and parse data
+  // res.json({ key2: "value2" }); // send data format json
+  // res.sendStatus(500); // send errors status message
+  res.sendFile(
+    path.join(__dirname, "index.html"),
+    {
+      headers: {
+        "content-type": "text/html",
+      },
+    },
+    (err) => {
+      if (err) {
+        res.sendStatus(500);
+      }
+    }
+  );
 });
 
 app.listen(3000);
