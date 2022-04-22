a1a2c0cd980f71351f6e72282fd33e5b7120e58f
FARIDJADDA <faridjadda750@gmail.com>
2022-04-22 06:47:53 +0200

    Pug advanced view list and condition if and case

--- app.js
+++ app.js
@@ -6,7 +6,16 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.render("index", { name: "jean" });
+  res.render("index", {
+    name: "jean",
+    authentificated: true,
+    friends: 15,
+    products: [
+      { title: "product1", content: "content1" },
+      { title: "product2", content: "content2" },
+      { title: "product3", content: "content3" },
+    ],
+  });
 });
 
 app.listen(3000);
