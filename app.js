0207ac1f91d5810c984939f23a3547e2e84d1d60
FARIDJADDA <faridjadda750@gmail.com>
2022-04-25 11:14:39 +0200

    Uses Talend Api and test middleware json and urlencoded

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body.key1);
+  console.log(typeof req.body);
   res.render("index");
 });
 
